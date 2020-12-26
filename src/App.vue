591a8bbbcd40c9c4c468d6a550bc185a4422b382
Sam Goodger <sam.goodger@growsuper.com>
2020-12-26 10:00:45 +1000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="@/assets/manchas001.jpg" alt="Manchas">
+    <div class="intro">
+      <p>Hello, my name is</p>
+      <h1>Manchas :3</h1>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+html, body {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  display: flex;
+  font-family: Poppins, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #FFF;
+  background: #000;
+}
+
+#app {
+  width: 100%;
+  display: flex;
+  position: relative;
+  align-items: center;
+}
+
+#app > img {
+  position: absolute;
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+}
+
+.intro {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 50%;
+  padding: 8rem;
+  font-size: 2rem;
+}
+
+h1, p {
+  margin: 0;
 }
 </style>
